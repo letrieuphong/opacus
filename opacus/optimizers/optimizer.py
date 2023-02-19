639131c7a7e3b2526cd6a4614dd477aefff06a5c
letrieuphong <letrieu.letrieuphong@gmail.com>
2023-02-19 19:16:57 +0900

    >0.25

--- opacus/optimizers/optimizer.py
+++ opacus/optimizers/optimizer.py
@@ -446,7 +446,7 @@ class DPOptimizer(Optimizer):
         if self.loss_reduction == "mean":
             for p in self.params:
                 p.grad /= self.expected_batch_size * self.accumulated_iterations
-                p.grad *= (torch.rand_like(p.grad) > 0.5)
+                p.grad *= (torch.rand_like(p.grad) > 0.25)
 
 
     def zero_grad(self, set_to_none: bool = False):
